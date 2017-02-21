366ce78ed0e6202968aa2689ae8dd80b699d666d
alexchep <alexchep92@gmail.com>
2017-02-21 23:54:51 +0200

    мелкие правки

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=furniture',
     'username' => 'root',
-    'password' => 'ey82h650',
+    'password' => '',
     'charset' => 'utf8',
 ];
--- modules/admin/views/comment/_form.php
+++ modules/admin/views/comment/_form.php
@@ -5,24 +5,24 @@ use yii\widgets\ActiveForm;
 
 ?>
 
-<div class="comments-form">
+<div class="comment-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'text')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'text')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'author_name')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'author_name')->textInput(['maxlength' => true]) ?>
 
-    <?php
-        echo $form->field($model, 'status')->dropDownList([
-            'Активен' => 'Активен',
-            'Неактивен' => 'Неактивен'
-        ]);
-    ?>
+        <?php
+            echo $form->field($model, 'status')->dropDownList([
+                'Активен' => 'Активен',
+                'Неактивен' => 'Неактивен'
+            ]);
+        ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Создать' : 'Сохранить', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
 
     <?php ActiveForm::end(); ?>
 
